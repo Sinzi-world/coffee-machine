266703a50ff56ed8c4f851355f0951c77f1c1fd1
Aleksey <lekha.bolt2004@gmail.com>
2025-01-31 16:00:09 +0300

    Start project

---
+++ src/main/java/com/example/coffemachine/CoffeMachineApplication.java
@@ -0,0 +1,13 @@
+package com.example.coffemachine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffeMachineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoffeMachineApplication.class, args);
+	}
+
+}
